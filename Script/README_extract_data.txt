ab656898fa5989dfd455bab4af47f30c9af3ac16
unknown <elamri.ilyas@gmail.com>
2020-02-18 16:10:01 +0100

    update

---
+++ Script/README_extract_data.txt
@@ -0,0 +1,3 @@
+To extract data for images through the command line, we can pass the following command :
+
+python extract_data.py ../Data ../Data/train2/train_png ../Data/dev2/dev_png ../Data/train2/xml ../Data/dev2/xml
--- Script/extract_data.py
+++ Script/extract_data.py
@@ -13,6 +13,7 @@ import os
 from skimage import color
 import xml.etree.ElementTree as ET
 import sys
+import time
 
 def extract_images(trainPATH,devPATH):
 
@@ -29,7 +30,6 @@ def extract_images(trainPATH,devPATH):
     Xdev = np.zeros(((1110,500,250)))
     
     for i, element in enumerate (os.listdir(trainPATH)):
-        print(i)
         filename_w_ext = os.path.basename(element)
         filename, file_extension = os.path.splitext(filename_w_ext)
         trainfilename.append(filename[0:6])
@@ -40,7 +40,6 @@ def extract_images(trainPATH,devPATH):
         Xtrain[i] = resize_image
         
     for i, element in enumerate (os.listdir(devPATH)):
-        print(i)
         filename_w_ext = os.path.basename(element)
         filename, file_extension = os.path.splitext(filename_w_ext)
         devfilename.append(filename[0:6])
@@ -54,7 +53,6 @@ def extract_images(trainPATH,devPATH):
 
 def get_class(trainfilename, devfilename, dftrain, dfdev):
     
-    
     ''' Input : - trainfilename : name of all images in train folder
                 - devfilename : name of all images in dev folder
                 - dftrain : dataframe of train csv file 
@@ -65,25 +63,39 @@ def get_class(trainfilename, devfilename, dftrain, dfdev):
         
     filetrain = dftrain['Filename']
     ytraincsv = dftrain['Class']
-    ytrain = []
-    
-    for i in range(len(trainfilename)):
-        print(i)
-        for j in range(len(filetrain)):   
-            if trainfilename[i] in filetrain[j]:
-                indtrain = np.where(filetrain == '../Data/train2/txt/' + trainfilename[i] + '.txt')[0][0]
-                ytrain.append(ytraincsv[indtrain])
-        
     filedev = dfdev['Filename']
     ydevcsv = dfdev['Class']
-    ydev = []
     
+    ytrain = []
+    for i in range(len(trainfilename)):
+        for j in range(len(filetrain)):   
+            if trainfilename[i] in filetrain[j]:
+                ytrain.append(ytraincsv[j])
+    
+    
+    fichier = open(sys.argv[1] + "/missed_data.txt", "a")
+    fichier.write("missed data in train png dataset \n")
+    count = 0
+    for i in range(len(filetrain)):
+        if filetrain[i][:6] not in trainfilename:
+            count+=1
+            fichier.write(filetrain[i][:6]+'\n')
+    fichier.write('number of element: ' + str(count) + '\n')
+    count = 0
+    fichier.write("\nmissed data in dev png dataset \n")
+    for i in range(len(filedev)):
+        if filedev[i][:6] not in devfilename:
+            count+=1
+            fichier.write(filedev[i][:6]+'\n')
+    fichier.write('number of element: ' + str(count) + '\n')
+    fichier.close() 
+        
+    
+    ydev = []
     for i in range(len(devfilename)):
-        print(i)
         for j in range(len(filedev)):   
             if devfilename[i] in filedev[j]:
-                inddev = np.where(filedev == '../Data/dev2/txt/' + devfilename[i] + '.txt')[0][0]
-                ydev.append(ydevcsv[inddev])
+                ydev.append(ydevcsv[j])
     
     classes = ['C1','C2','C3','C4','C5']
     
@@ -113,22 +125,18 @@ def get_language(trainfilename, devfilename, dftrain, dfdev):
     ytrain = []
     
     for i in range(len(trainfilename)):
-        print(i)
         for j in range(len(filetrain)):   
             if trainfilename[i] in filetrain[j]:
-                indtrain = np.where(filetrain == '../Data/train2/txt/' + trainfilename[i] + '.txt')[0][0]
-                ytrain.append(ytraincsv[indtrain])
+                ytrain.append(ytraincsv[j])
         
     filedev = dfdev['Filename']
     ydevcsv = dfdev['Language']
     ydev = []
     
     for i in range(len(devfilename)):
-        print(i)
         for j in range(len(filedev)):   
             if devfilename[i] in filedev[j]:
-                inddev = np.where(filedev == '../Data/dev2/txt/' + devfilename[i] + '.txt')[0][0]
-                ydev.append(ydevcsv[inddev])
+                ydev.append(ydevcsv[j])
     
     classes = ['fr','ar','en']
     
@@ -157,7 +165,6 @@ def get_type(trainPATH,devPATH,trainfilename,devfilename):
     documents_type = []
     filenames_train = []
     for i, element in enumerate (os.listdir(trainPATH)):
-        print(i)
         filename_w_ext = os.path.basename(element)
         filename, file_extension = os.path.splitext(filename_w_ext)
         filenames_train.append(filename)
@@ -187,7 +194,6 @@ def get_type(trainPATH,devPATH,trainfilename,devfilename):
     documents_type = []
     filenames_dev = []
     for i, element in enumerate (os.listdir(devPATH)):
-        print(i)
         filename_w_ext = os.path.basename(element)
         filename, file_extension = os.path.splitext(filename_w_ext)
         filenames_dev.append(filename)
@@ -218,37 +224,70 @@ def get_type(trainPATH,devPATH,trainfilename,devfilename):
 #%% 
 
 # For instance, we can pass arguments through the command line :
-# python extract_data.py ../Data/classes.csv  ../Data/classes_dev.csv  ../Data/train2/train_png  ../Data/dev2/dev_png  ../Data/train2/xml  ../Data/dev2/xml
+# python extract_data.py ../Data  ../Data/train2/train_png  ../Data/dev2/dev_png  ../Data/train2/xml  ../Data/dev2/xml
     
 if __name__ == "__main__":
     # load dataframe from CSV file
-    dftrain = pd.read_csv(sys.argv[1] + '/classes.csv', sep="\t")
-    dfdev = pd.read_csv(sys.argv[1] + '/classes_dev.csv', sep="\t")
-    
+    dftrain = pd.read_csv(sys.argv[1] + '/classes.csv', sep=",")
+    dfdev = pd.read_csv(sys.argv[1] + '/classes_dev.csv', sep=",")
     # Path of images
     imgtrainPATH = sys.argv[2]
     imgdevPATH = sys.argv[3]
-    
     # Path of xmls
     xmltrainPATH = sys.argv[4]
     xmldevPATH = sys.argv[5]
-    
     # extract images
-    Xtrain, Xdev, trainfilename , devfilename = extract_images(imgtrainPATH,imgdevPATH)
-    
+    print('------------------------------------------------------------------')
+    print('Extracting images from train and dev folders')
+    print('Used folders:\t', imgtrainPATH, '\t', imgdevPATH)
+    print('!! This will take few minutes !!')
+    t = time.time()
+    Xtrain, Xdev, trainfilename, devfilename = extract_images(imgtrainPATH,
+                                                               imgdevPATH)
+    print('Execution time:', '{:^10.2f}'.format((time.time()-t)/60), ' min')
     # get class
-    ytrain_class, ydev_class = get_class(trainfilename, devfilename, dftrain, dfdev)
-    
-    # get_lLanguage
-    ytrain_language, ydev_language = get_language(trainfilename, devfilename, dftrain, dfdev)
-    
+    print('------------------------------------------------------------------')
+    print('Getting class of images')
+    print('Used folders:\t', sys.argv[1])
+    print('!! This will take few minutes !!')
+    t = time.time()
+    ytrain_class, ydev_class = get_class(trainfilename,
+                                         devfilename,
+                                         dftrain,
+                                         dfdev)
+    print('missed data are saved in :\t', sys.argv[1])
+    print('Execution time:', '{:^10.2f}'.format((time.time()-t)/60), ' min')
+    # get_Language
+    print('------------------------------------------------------------------')
+    print('Getting language of images')
+    print('Used folders:\t', sys.argv[1])
+    print('!! This will take few minutes !!')
+    t = time.time()
+    ytrain_language, ydev_language = get_language(trainfilename,
+                                                  devfilename,
+                                                  dftrain,
+                                                  dfdev)
+    print('Execution time:', '{:^10.2f}'.format((time.time()-t)/60), ' min')
     # get_type
-    ytrain_type, ydev_type = get_type(xmltrainPATH,xmldevPATH,trainfilename,devfilename)
-    
+    print('------------------------------------------------------------------')
+    print('Getting type of images')
+    print('Used folders:\t', xmltrainPATH, '\t', xmldevPATH)
+    print('!! This will take few minutes !!')
+    t = time.time()
+    ytrain_type, ydev_type = get_type(xmltrainPATH,
+                                      xmldevPATH,
+                                      trainfilename,
+                                      devfilename)
+    print('Execution time:', '{:^10.2f}'.format((time.time()-t)/60), ' min')
     # normalization
-    Xtrain=(Xtrain-np.mean(Xtrain, axis=0))/np.std(Xtrain, axis=0)
-    Xdev=(Xdev-np.mean(Xdev, axis=0))/np.std(Xdev, axis=0)
-    
+    Xtrain = (Xtrain-np.mean(Xtrain, axis=0))/np.std(Xtrain, axis=0)
+    Xdev = (Xdev-np.mean(Xdev, axis=0))/np.std(Xdev, axis=0)
     # Saving data into npz file
+    print('------------------------------------------------------------------')
+    print('Saving data into npz file in:\t', sys.argv[1])
     filenpz = sys.argv[1] + '/data'
-    np.savez(filenpz,Xtrain=Xtrain,ytrain_class=ytrain_class,Xdev=Xdev,ydev_class=ydev_class, ytrain_language=ytrain_language,ydev_language=ydev_language,ytrain_type=ytrain_type,ydev_type=ydev_type)
+    t = time.time()
+    np.savez(filenpz, Xtrain=Xtrain, ytrain_class=ytrain_class, Xdev=Xdev,
+             ydev_class=ydev_class, ytrain_language=ytrain_language,
+             ydev_language=ydev_language, ytrain_type=ytrain_type,
+             ydev_type=ydev_type)
